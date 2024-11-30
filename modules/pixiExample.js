c9e316a8728cb4e93fceaa2eb9f2c790e717491a
Matthew King <146943749+MattEKing@users.noreply.github.com>
2024-11-30 16:19:19 -0330

    - Fixed the CSS file location - did some mockup styling - got the canvas locating in the main of the page for styling purposes

--- modules/pixiExample.js
+++ modules/pixiExample.js
@@ -1,16 +1,28 @@
 ( async () => {
     const app = new PIXI.Application();
 
-    await app.init({ width: window.innerWidth, height: window.innerHeight });
+    await app.init({background: "#206b35" ,width: 1000, height: 900});
 
-    document.body.appendChild(app.canvas);
+    document.getElementById("gameBox").appendChild(app.canvas);
+
     // load the PNG asynchronously
     await PIXI.Assets.load('sprites/FinishSprites/pixel-art-dungeon-.jpg');
-        let sprite1 = PIXI.Sprite.from('sprites/FinishSprites/pixel-art-dungeon-.jpg');
-        app.stage.addChild(sprite1);
+        let cave = PIXI.Sprite.from('sprites/FinishSprites/pixel-art-dungeon-.jpg');
+        app.stage.addChild(cave);
+        // center the sprite when it loads. also anchor fixes where to center of the sprites is
+        cave.anchor.set(0.5);
+        cave.x = app.screen.width / 2;
+        cave.y = app.screen.height / 2;
+
     await PIXI.Assets.load('sprites/FinishSprites/BatEyeEnemy.png');
-    let sprite = PIXI.Sprite.from('sprites/FinishSprites/BatEyeEnemy.png');
-    app.stage.addChild(sprite);
+        const bat = PIXI.Sprite.from('sprites/FinishSprites/BatEyeEnemy.png');
+        app.stage.addChild(bat);
+        //center bat on load and fixes sprite center with anchor.
+        bat.anchor.set(0.5);
+        bat.x = app.screen.width / 2;
+        bat.y = app.screen.height / 2;
+    
+
     // Add a variable to count up the seconds our demo has been running
     let elapsed = 0.0;
     // Tell our application's ticker to run a new callback every frame, passing
@@ -18,8 +30,10 @@
     app.ticker.add((ticker) => {
         // Add the time to our total elapsed time
         elapsed += ticker.deltaTime;
+
         // Update the sprite's X position based on the cosine of our elapsed time.  We divide
         // by 50 to slow the animation down a bit...
-        sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+        // bat now flys back and forth on the stage.
+        bat.x = 500.0 + Math.cos(elapsed/50.0) * 100.0;
     });
 })();
