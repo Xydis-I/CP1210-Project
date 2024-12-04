d2cf2e1f566183eb31499d60da9a58680fa53e08
Matthew King <146943749+MattEKing@users.noreply.github.com>
2024-12-03 22:10:31 -0330

    Added the main menu page and finished the styling and fixed routing problems.

---
+++ modules/changePage.js
@@ -0,0 +1,18 @@
+
+
+
+// Might pull out all the page changing later but it works for now just in the html script. -matt
+
+
+
+
+// const $ = (selector) => document.querySelector(selector);
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     $("#start").addEventListener('click', changePage)
+// });
+
+// const changePage = () => {
+//     const path = document.getAttribute('pagepath');
+//     location.replace(location.pathname = path);
+//     }
