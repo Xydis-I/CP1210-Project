2c854b3a167d684d614efc531273ad8d1a0e86af
Matthew King <146943749+MattEKing@users.noreply.github.com>
2024-12-05 15:27:45 -0330

    Add click logic for correct and incorrect guesses

--- modules/game.js
+++ modules/game.js
@@ -20,15 +20,15 @@
     const questions = [
         {question:"What is the name of the latest Nintendo system?", answer1: "Playstation", answer2: "Switch", answer3: "Xbox", answer4: "Wii", correct: "Switch"},
         {question:"When did the first Super mario bros release?", answer1: "1985", answer2: "1984", answer3: "1986", answer4: "1987"},
-        {question:"What was the first Souls-like game Developed", answer1: "Dark Soul's", answer2: "Bloodborne", answer3: "Elden Ring", answer4: "Demon Soul's", correct: "Demon soul's" },
+        {question:"What was the first Souls-like game Developed?", answer1: "Dark Soul's", answer2: "Bloodborne", answer3: "Elden Ring", answer4: "Demon Soul's", correct: "Demon soul's" },
         {question:"Who is the main character of The Legend of Zelda series?", answer1: "Zelda", answer2: "Ganon", answer3: "Link", answer4: "Tingle", correct: "Link"},
         {question:"What company publised the Metal Gear Series?", answer1: "Konami", answer2: "Square Enix", answer3: "Nintendo", answer4: "Kojima", correct: "Konami"},
         {question:"What game's main character is named Chel?", answer1: "Half-Life", answer2: "TF2", answer3: "Portal", answer4: "Deadlock", correct: "Portal"},
         {question:"What W.O.W. characters title is 'Warchilf of the Horde?'", answer1: "Thrall", answer2: "Rexxar", answer3: "Illidan", answer4: "BlackHand", correct: "Thrall"},
         {question:"What game series are the 'Blades of chaos from?'", answer1: "God of War", answer2: "Diablo", answer3: "Dark soul's", answer4: "Hades", correct: "God of War"},
-        {question:"What is the 151 pokemon in the pokedex", answer1: "Pikachu", answer2: "Mew", answer3: "Mewtwo", answer4: "Bulbsaur", correct: "Mew"},
-        {question:"What elder Scrolls game was released in 2011", answer1: "Daggerfall", answer2: "Oblivion", answer3: "Morrowind", answer4: "Skyrim", correct: "Skyrim"},
-        {question:"What was Toby Fox's first game", answer1: "EarthBound", answer2: "Deltarune", answer3: "Undertale", answer4: "Celeste", correct: "Undertale"},
+        {question:"What is the 151 pokemon in the pokedex?", answer1: "Pikachu", answer2: "Mew", answer3: "Mewtwo", answer4: "Bulbsaur", correct: "Mew"},
+        {question:"What elder Scrolls game was released in 2011?", answer1: "Daggerfall", answer2: "Oblivion", answer3: "Morrowind", answer4: "Skyrim", correct: "Skyrim"},
+        {question:"What was Toby Fox's first game?", answer1: "EarthBound", answer2: "Deltarune", answer3: "Undertale", answer4: "Celeste", correct: "Undertale"},
         {question:"What game has a loonie tunes art style?", answer1: "Cuphead", answer2: "It takes Two", answer3: "Noita", answer4: "Pikmin", correct: "Cuphead"},
         {question:"what series does the Character 'Samus' come from?", answer1: "mass effect", answer2: "F-Zero", answer3: "Metroid", answer4: "StarFox", correct: "Metroid"},
         {question:"Cloud is the main character of what Final fantasy?", answer1: "VIII(8)", answer2: "VII(7)", answer3: "X(10)", answer4: "VI(6)", correct: "VII(7)"},
@@ -136,6 +136,14 @@
             answer1.style = defaultStyle;
             answer1.scale = 1;
         });
+        answer1.on('click', () => {
+            if (answer1.text == questions[questionNumber].correct) {
+                console.log("correct")
+            }
+            if (answer1.text != questions[questionNumber].correct) {
+                console.log("incorrect")
+            }
+        });
 
         const answer2 = new PIXI.Text({ text: 'Answer2', style: defaultStyle });
         answer2.anchor.set(0.5);
@@ -157,6 +165,14 @@
             answer2.scale = 1;
 
         });
+        answer2.on('click', () => {
+            if (answer2.text == questions[questionNumber].correct) {
+                console.log("correct")
+            }
+            if (answer2.text != questions[questionNumber].correct) {
+                console.log("incorrect")
+            }
+        });
 
         const answer3 = new PIXI.Text({ text: 'Answer3', style: defaultStyle });
         answer3.anchor.set(0.5);
@@ -176,6 +192,14 @@
             answer3.style = defaultStyle;
             answer3.scale = 1;
         });
+        answer3.on('click', () => {
+            if (answer3.text == questions[questionNumber].correct) {
+                console.log("correct")
+            }
+            if (answer3.text != questions[questionNumber].correct) {
+                console.log("incorrect")
+            }
+        });
 
         const answer4 = new PIXI.Text({ text: 'Answer4', style: defaultStyle });
         answer4.anchor.set(0.5);
@@ -194,6 +218,14 @@
         answer4.on('pointerout', () => {
             answer4.style = defaultStyle;
             answer4.scale = 1;
+        });
+        answer4.on('click', () => {
+            if (answer4.text == questions[questionNumber].correct) {
+                console.log("correct")
+            }
+            if (answer4.text != questions[questionNumber].correct) {
+                console.log("incorrect")
+            }
         });
 
     await PIXI.Assets.load('../sprites/UI/LevelMap.png');
@@ -235,7 +267,6 @@
             clearInterval(levelInterval);
         }
     }
-
 
     let elapsed = 0.0;
     app.ticker.add((ticker) => {
